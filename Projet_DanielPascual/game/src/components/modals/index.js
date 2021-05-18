b938bbe3dd28873e4f2ccfe284b13bf65901e4ad
blueDPS <e.dpascualsiliezar@etu.cvm.qc.ca>
2021-05-18 15:58:57 -0400

    removing js files

--- Projet_DanielPascual/game/src/components/modals/index.js
+++
@@ -1,5 +0,0 @@
-import UsernameSetup from "./UsernameSetup.svelte";
-import GameSetup from "./GameSetup.svelte";
-import Waiting from "./Waiting.svelte";
-export { UsernameSetup, GameSetup, Waiting };
-//# sourceMappingURL=index.js.map
