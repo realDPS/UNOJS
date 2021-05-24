5515a28ce5ad172828ecd25c61ee22634592563b
blueDPS <e.dpascualsiliezar@etu.cvm.qc.ca>
2021-05-24 12:57:43 -0400

    added conditional for vite.js bundler

--- Projet_DanielPascual/game/vite.config.js
+++ Projet_DanielPascual/game/vite.config.js
@@ -1,22 +1,37 @@
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svelte from "@sveltejs/vite-plugin-svelte";
 import strip from "@rollup/plugin-strip";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    svelte(),
-    tsconfigPaths(),
-    strip({ include: ["**/*.js", "**/*.svelte", "**/*.ts"] })
-  ],
-  build: {
-    outDir: "build",
-    terserOptions: {
-      compress: true
-    }
-  },
-  server: {
-    port: 5000
+export default ({ _command, mode }) => {
+  if (mode === "development") {
+    return {
+      plugins: [svelte(), tsconfigPaths()],
+      build: {
+        outDir: "build",
+        terserOptions: {
+          compress: true
+        }
+      },
+      server: {
+        port: 5000
+      }
+    };
+  } else {
+    return {
+      plugins: [
+        svelte(),
+        tsconfigPaths(),
+        strip({ include: ["**/*.js", "**/*.svelte", "**/*.ts"] })
+      ],
+      build: {
+        outDir: "build",
+        terserOptions: {
+          compress: true
+        }
+      },
+      server: {
+        port: 5000
+      }
+    };
   }
-});
+};
