7adde4bee1271ed74f27db0b0910affb655cde46
realDPS <pascualbzg7@gmail.com>
2025-05-11 02:35:08 -0400

    reorganize folder structure

--- Projet_DanielPascual/game/src/components/modals/index.ts
+++
@@ -1,6 +0,0 @@
-import UsernameSetup from "./UsernameSetup.svelte";
-import GameSetup from "./GameSetup.svelte";
-import Waiting from "./Waiting.svelte";
-import Winner from "./Winner.svelte";
-
-export { UsernameSetup, GameSetup, Waiting, Winner };
